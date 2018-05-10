b6013fce88b76da1a2c0118fcdba535d73d0613b
Sumant Sharma <smntshrma@gmail.com>
2018-05-10 16:08:27 -0700

    fixing comment indentations

--- me227_controller.m
+++ me227_controller.m
@@ -47,29 +47,28 @@ function [delta_rad, Fx_N] = me227_controller(s_m, e_m, deltaPsi_rad, Ux_mps, Uy
     state.Uy_mps = Uy_mps;
     state.r_radps = r_radps;
     
-    %% Create the vehicle object that holds relevant parameters
     % Setting parameters from the provided data sheet
-    veh.m = 1659;           % mass [kg]
-    veh.L = 2.468;          % wheelbase [m]
-    veh.Iz = 2447;          % out-of-plane MOI [kg m^2]
-    veh.perc_W_f = 0.577;   % percent weight on front [fractional]
-    veh.f_rr = 0.0157;      % coefficient of rolling resistance
-    veh.CdA = 0.594;        % drag coeff. * drag area [m^2]
-    veh.Caf_lin = 188e03;   % linear front tire stiffness [N/rad]
-    veh.Car_lin = 203e03;   % linear rear tire stiffness [N/rad]
-    veh.mu_f = 0.97;        % cofficient of friction (front)
-    veh.mu_fs = 0.97;       % cofficient of friction (front)
-    veh.mu_r = 1.03;        % cofficient of friction (rear)
-    veh.mu_rs = 1.03;       % cofficient of friction (rear)
-    veh.Caf = 275e03;       % nonlinear front tire stiffness [N/rad]
-    veh.Car = 265e03;       % nonlinear rear tire stiffness [N/rad]
+    veh.m = 1659;                                                   % mass [kg]
+    veh.L = 2.468;                                                  % wheelbase [m]
+    veh.Iz = 2447;                                                  % out-of-plane MOI [kg m^2]
+    veh.perc_W_f = 0.577;                                           % percent weight on front [fractional]
+    veh.f_rr = 0.0157;                                              % coefficient of rolling resistance
+    veh.CdA = 0.594;                                                % drag coeff. * drag area [m^2]
+    veh.Caf_lin = 188e03;                                           % linear front tire stiffness [N/rad]
+    veh.Car_lin = 203e03;                                           % linear rear tire stiffness [N/rad]
+    veh.mu_f = 0.97;                                                % cofficient of friction (front)
+    veh.mu_fs = 0.97;                                               % cofficient of friction (front)
+    veh.mu_r = 1.03;                                                % cofficient of friction (rear)
+    veh.mu_rs = 1.03;                                               % cofficient of friction (rear)
+    veh.Caf = 275e03;                                               % nonlinear front tire stiffness [N/rad]
+    veh.Car = 265e03;                                               % nonlinear rear tire stiffness [N/rad]
     % Computing dependent parameters
-    veh.W = veh.m * 9.81;                       % vehicle weight (m*g) [N]
-    veh.Wf = veh.perc_W_f * veh.W;              % vehicle front weight [N]
-    veh.Wr = veh.W - veh.Wf;                    % vehicle rear weight [N]
-    veh.perc_W_r = 1 - veh.perc_W_f;            % percentage weight on rear [fractional]
-    veh.a = veh.L * veh.perc_W_r;       % distance from front wheel to CG [m]
-    veh.b = veh.L * veh.perc_W_f;       % distance from rear wheel to CG [m]
+    veh.W = veh.m * 9.81;                                           % vehicle weight (m*g) [N]
+    veh.Wf = veh.perc_W_f * veh.W;                                  % vehicle front weight [N]
+    veh.Wr = veh.W - veh.Wf;                                        % vehicle rear weight [N]
+    veh.perc_W_r = 1 - veh.perc_W_f;                                % percentage weight on rear [fractional]
+    veh.a = veh.L * veh.perc_W_r;                                   % distance from front wheel to CG [m]
+    veh.b = veh.L * veh.perc_W_f;                                   % distance from rear wheel to CG [m]
     % Computing understeer gradient [rad/m/s^2]
     veh.K = veh.m * ((veh.perc_W_f / veh.Caf) - (veh.perc_W_r / veh.Car));
     % Computing the characteristic speed [m/s]
@@ -83,10 +82,9 @@ function [delta_rad, Fx_N] = me227_controller(s_m, e_m, deltaPsi_rad, Ux_mps, Uy
     % initialize dt
     dt = 0.01;
     
-    %% Controllers
+    % Controllers
     if modeSelector == 1
-    % Runs the feedforward lookahead lateral controller with a
-    % feedfoward/PI-feedback longitudinal controller.
+    % Runs the FF lookahead lateral controller with a feedfoward/PI-feedback longitudinal controller.
         
         % Lateral control law
         K_la = 3500;
@@ -101,7 +99,9 @@ function [delta_rad, Fx_N] = me227_controller(s_m, e_m, deltaPsi_rad, Ux_mps, Uy
         alpha_f = atan2(state.Uy_mps + (veh.a * state.r_radps), state.Ux_mps) - delta_rad;
         alpha_slip_f = abs(atan2(3 * veh.mu_f * veh.Wf, veh.Caf));
         if abs(alpha_f) < alpha_slip_f
-            Fyf_N = helperFialaTireForce(veh.Caf, alpha_f, veh.mu_f, veh.mu_fs, veh.Wf);
+            Fyf_N = (-veh.Caf * tan(alpha_f)) + ...
+                    (((veh.Caf^2) / (3 * veh.mu_f * veh.Wf) * (2 - (veh.mu_fs / veh.mu_f))) * abs(tan(alpha_f)) *tan(alpha_f)) - ...
+                    (((veh.Caf^3) / (9 * veh.mu_f * veh.mu_f * veh.Wf * veh.Wf)) * (tan(alpha_f)^3) * (1 - (2 * veh.mu_fs / 3 / veh.mu_f)));
         else
             Fyf_N = -veh.mu_fs * veh.Wf * sign(alpha_f);
         end
@@ -112,22 +112,21 @@ function [delta_rad, Fx_N] = me227_controller(s_m, e_m, deltaPsi_rad, Ux_mps, Uy
         error = (pathPlan.Ux_des_mps - state.Ux_mps);
         integrated_Ux_error = integrated_Ux_error + (error * dt);
         if abs(error) < 0.5 
-            integrated_Ux_error = 0;    % Setting integrated error threshold on Ux_des (~ 1 mph or 0.5 m/s) to avoid integrator windup.
+            integrated_Ux_error = 0;                                    % Setting integrated error threshold on Ux_des (~ 1 mph or 0.5 m/s) to avoid integrator windup.
         end
         Fx_fb_N = (K_p * error) + (K_i * integrated_Ux_error);
-        rho = 1.225;                                        % atmo density [km/m^3]
-        Fdrag = 0.5 * rho * veh.CdA * state.Ux_mps^2;       % drag force [N]
-        Ux_rr_thresh_mps = 1;                               % Setting threshold
+        rho = 1.225;                                                    % atmo density [km/m^3]
+        Fdrag = 0.5 * rho * veh.CdA * state.Ux_mps^2;                   % drag force [N]
+        Ux_rr_thresh_mps = 1;                                           % Setting threshold
         Frr = (veh.f_rr * veh.W) * (state.Ux_mps > Ux_rr_thresh_mps);   % Computing rolling resistance force, which only acts when the velocity is greater than Ux_rr_thresh_mps
-        Fx_ffw_N = Fdrag + Frr + ...    % Accounting for disturbance forces
-                   (veh.m * pathPlan.Ux_dot_des_mps2) + ... % Desired longitudinal accel.
-                   (Fyf_N * sin(delta_rad)) - ...           % Dynamical coupling
-                   (veh.m * state.r_radps * state.Uy_mps);  % Dynamical coupling
+        Fx_ffw_N = Fdrag + Frr + ...                                    % Accounting for disturbance forces
+                   (veh.m * pathPlan.Ux_dot_des_mps2) + ...             % Desired longitudinal accel.
+                   (Fyf_N * sin(delta_rad)) - ...                       % Dynamical coupling
+                   (veh.m * state.r_radps * state.Uy_mps);              % Dynamical coupling
         Fx_N = Fx_fb_N + Fx_ffw_N;
         
     elseif modeSelector == 2
-    % Runs the lateral PID controller with a feedforward/PI-feedback
-    % longitudinal controller.
+    % Runs the lateral PD controller with a feedforward/PI-feedback longitudinal controller.
         
         % Lateral control law (compute delta based on PD control law)
         if  isempty(prev_e_m)
@@ -142,7 +141,9 @@ function [delta_rad, Fx_N] = me227_controller(s_m, e_m, deltaPsi_rad, Ux_mps, Uy
         alpha_f = atan2(state.Uy_mps + (veh.a * state.r_radps), state.Ux_mps) - delta_rad;
         alpha_slip_f = abs(atan2(3 * veh.mu_f * veh.Wf, veh.Caf));
         if abs(alpha_f) < alpha_slip_f
-            Fyf_N = helperFialaTireForce(veh.Caf, alpha_f, veh.mu_f, veh.mu_fs, veh.Wf);
+            Fyf_N = (-veh.Caf * tan(alpha_f)) + ...
+                    (((veh.Caf^2) / (3 * veh.mu_f * veh.Wf) * (2 - (veh.mu_fs / veh.mu_f))) * abs(tan(alpha_f)) *tan(alpha_f)) - ...
+                    (((veh.Caf^3) / (9 * veh.mu_f * veh.mu_f * veh.Wf * veh.Wf)) * (tan(alpha_f)^3) * (1 - (2 * veh.mu_fs / 3 / veh.mu_f)));
         else
             Fyf_N = -veh.mu_fs * veh.Wf * sign(alpha_f);
         end
@@ -153,28 +154,19 @@ function [delta_rad, Fx_N] = me227_controller(s_m, e_m, deltaPsi_rad, Ux_mps, Uy
         error = (pathPlan.Ux_des_mps - state.Ux_mps);
         integrated_Ux_error = integrated_Ux_error + (error * dt);
         if abs(error) < 0.5 
-            integrated_Ux_error = 0;    % Setting integrated error threshold on Ux_des (~ 1 mph or 0.5 m/s) to avoid integrator windup.
+            integrated_Ux_error = 0;                                    % Setting integrated error threshold on Ux_des (~ 1 mph or 0.5 m/s) to avoid integrator windup.
         end
         Fx_fb_N = (K_p * error) + (K_i * integrated_Ux_error);
-        rho = 1.225;                                        % atmo density [km/m^3]
-        Fdrag = 0.5 * rho * veh.CdA * state.Ux_mps^2;       % drag force [N]
-        Ux_rr_thresh_mps = 1;                               % Setting threshold
+        rho = 1.225;                                                    % atmo density [km/m^3]
+        Fdrag = 0.5 * rho * veh.CdA * state.Ux_mps^2;                   % drag force [N]
+        Ux_rr_thresh_mps = 1;                                           % Setting threshold
         Frr = (veh.f_rr * veh.W) * (state.Ux_mps > Ux_rr_thresh_mps);   % Computing rolling resistance force, which only acts when the velocity is greater than Ux_rr_thresh_mps
-        Fx_ffw_N = Fdrag + Frr + ...    % Accounting for disturbance forces
-                   (veh.m * pathPlan.Ux_dot_des_mps2) + ... % Desired longitudinal accel.
-                   (Fyf_N * sin(delta_rad)) - ...           % Dynamical coupling
-                   (veh.m * state.r_radps * state.Uy_mps);  % Dynamical coupling
+        Fx_ffw_N = Fdrag + Frr + ...                                    % Accounting for disturbance forces
+                   (veh.m * pathPlan.Ux_dot_des_mps2) + ...             % Desired longitudinal accel.
+                   (Fyf_N * sin(delta_rad)) - ...                       % Dynamical coupling
+                   (veh.m * state.r_radps * state.Uy_mps);              % Dynamical coupling
         Fx_N = Fx_fb_N + Fx_ffw_N;
         
     end
 
-end
-
-function Fy = helperFialaTireForce(Ca, alpha, mu, mu_s, W)
-% Helper function for computing Fy according to Fiala model
-                
-    Fy = (-Ca * tan(alpha)) + ...
-         (((Ca^2) / (3 * mu * W) * (2 - (mu_s / mu))) * abs(tan(alpha)) *tan(alpha)) - ...
-         (((Ca^3) / (9 * mu * mu * W * W)) * (tan(alpha)^3) * (1 - (2 * mu_s / 3 / mu)));
-            
 end
